a1db9353fb36beb525c1d991df9542e09deff20c
Gabriel Y. Honji <gabriel.honji78@gmail.com>
2023-05-12 15:38:56 -0300

    update home

--- pt-br/javascript/home-script.js
+++ pt-br/javascript/home-script.js
@@ -1,9 +1,9 @@
 const navEl = document.querySelector('.navjs');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 100) {
         navEl.classList.add('nav-scrolled');
-    } else if (window.scrollY <= 50 ) {
+    } else if (window.scrollY <= 100 ) {
         navEl.classList.remove('nav-scrolled');
     }
 });
