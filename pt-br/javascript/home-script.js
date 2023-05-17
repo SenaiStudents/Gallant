75f2dafa27547dbba0a6948eb132791f9273bee0
Gabriel Y. Honji <gabriel.honji78@gmail.com>
2023-05-17 15:33:16 -0300

    update home

--- pt-br/javascript/home-script.js
+++ pt-br/javascript/home-script.js
@@ -7,3 +7,17 @@ window.addEventListener('scroll', () => {
         navEl.classList.remove('nav-scrolled');
     }
 });
+
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {};
+  
+  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+  ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+  });
